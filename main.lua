c567ab2862735fd238e8af5a3534282f610eb7a7
SourojeetShyam <100289496+CoderSourojeet@users.noreply.github.com>
2022-02-23 14:24:34 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,20 @@
+local FirstNames = {
+    "Elphias",
+    "James",
+    "Robert"
+}
+
+local LastNames = {
+    "Doge",
+    "Charlie",
+    "Smithson"
+}
+  
+for count = 1,10 do
+ print(FirstNames[math.random(1,3)])
+ print(LastNames[math.random(1,3)])
+ print()
+end
+
+
+  
