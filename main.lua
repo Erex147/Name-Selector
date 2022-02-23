05321f6d493aa6688e2fa8e71f8c9460629f3207
Sourojeet Shyam <100289496+CoderSourojeet@users.noreply.github.com>
2022-02-23 17:49:06 +0000

    unified all functions and removed duplications

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-local FirstNames = {
+
+  local FirstNames = {
     "Elphias",
     "James",
     "Robert"
@@ -9,11 +10,7 @@ local LastNames = {
     "Charlie",
     "Smithson"
 }
-  
-for count = 1,10 do
- print(FirstNames[math.random(1,3)], LastNames[math.random(1,3)])
- print()
-end
+
 local function CustomName()
   io.write("Insert any First Name: ")
   print()
@@ -25,12 +22,30 @@ local function CustomName()
   table.insert(LastNames, lastName)
   local name = tostring(firstName)..(" ")..tostring(lastName)
   print("Added Custom Name Succesfully. Result: ".. name)
-  return name  
+  return name 
 end
-    
-io.write("Do you want to add a custom name? y/n :")
-choice = io.read()
+  
+local busy = false
+local function PrintNames()
+  for count = 1,10 do
+    print(FirstNames[math.random(1,#FirstNames)], 
+        LastNames[math.random(1,#LastNames)])
+        print()
+  end   
+end  
+while not busy do
+busy = true
+io.write("Do you want to print names?")
+local choice = io.read()
+ if choice == "y" then
+    PrintNames()
+ elseif choice == "n" then
+    io.write("Do you want to add a custom name? y/n :")
+local choice = io.read()
 if choice == "y"
 then
     CustomName()
 end  
+ end
+  busy = false
+end
