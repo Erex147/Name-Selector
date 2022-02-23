2a087dbf6bdabe389f04b0a03f9e4ac0a910ce99
Sourojeet Shyam <100289496+CoderSourojeet@users.noreply.github.com>
2022-02-23 15:45:19 +0000

    custom name but still in progress

--- main.lua
+++ main.lua
@@ -15,6 +15,20 @@ for count = 1,10 do
  print(LastNames[math.random(1,3)])
  print()
 end
-
-
+local function CustomName()
+  io.write("Insert any First Name")
+  local firstName = io.read()
+  table.insert(FirstNames, firstName)
+  io.write("Insert any Last Name")
+  local lastName = io.read()
+  table.insert(LastNames, lastName)
+  local name = tostring(firstName)..(" ")..tostring(lastName)
+  print(name)
+end
+io.write("Do youwant to add a custom name? y/n")
+bool = tostring(io.read)
+  if (bool = "y")
+  then
+    CustomName()
+end
   
